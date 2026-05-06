3aee648ac541f96e388ce3ef28a4966292fce65b
Naw7k <n_m_k0@hotmail.com>
2026-05-06 14:48:30 +0300

    Update DebugScreen.java

--- src/main/java/net/naw/morphling/client/debug/DebugScreen.java
+++ src/main/java/net/naw/morphling/client/debug/DebugScreen.java
@@ -33,11 +33,6 @@ public class DebugScreen extends Screen {
         ).bounds(centerX - 100, y + 25, 200, 20).build());
 
         this.addRenderableWidget(Button.builder(
-                Component.literal("Close"),
-                btn -> this.onClose()
-        ).bounds(centerX - 100, y + 100, 200, 20).build());
-
-        this.addRenderableWidget(Button.builder(
                 Component.literal("Speedometer: " + (DebugSettings.isSpeedometerEnabled() ? "ON" : "OFF")),
                 btn -> {
                     DebugSettings.setSpeedometerEnabled(!DebugSettings.isSpeedometerEnabled());
@@ -49,6 +44,16 @@ public class DebugScreen extends Screen {
                 Component.literal("Hand Placement Tuner"),
                 btn -> Minecraft.getInstance().setScreen(new HandPlacementDebugScreen())
         ).bounds(centerX - 100, y + 75, 200, 20).build());
+
+        this.addRenderableWidget(Button.builder(
+                Component.literal("Held Item Tuner"),
+                btn -> Minecraft.getInstance().setScreen(new HeldItemTunerScreen())
+        ).bounds(centerX - 100, y + 100, 200, 20).build());
+
+        this.addRenderableWidget(Button.builder(
+                Component.literal("Close"),
+                btn -> this.onClose()
+        ).bounds(centerX - 100, y + 125, 200, 20).build());
     }
 
     @Override
