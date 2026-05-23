159e8255cef7f674d1ad56a0f8dd0045870194e3
Naw7k <n_m_k0@hotmail.com>
2026-05-24 02:36:53 +0300

    Update SkeletonBowDropMixin.java

--- src/main/java/net/naw/morphling/mixin/skeleton/SkeletonBowDropMixin.java
+++ src/main/java/net/naw/morphling/mixin/skeleton/SkeletonBowDropMixin.java
@@ -6,7 +6,6 @@ import net.minecraft.world.entity.item.ItemEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.Items;
-import net.naw.morphling.client.abilities.SkeletonAbility;
 import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -18,7 +17,6 @@ public abstract class SkeletonBowDropMixin {
 
     @Inject(method = "drop(Lnet/minecraft/world/item/ItemStack;Z)Lnet/minecraft/world/entity/item/ItemEntity;", at = @At("HEAD"), cancellable = true)
     private void morphling$preventSkeletonBowDrop(ItemStack itemStack, boolean thrownFromHand, CallbackInfoReturnable<@Nullable ItemEntity> cir) {
-        if (!SkeletonAbility.isBowEquipped()) return;
         if (itemStack.getItem() != Items.BOW) return;
         Component name = itemStack.get(DataComponents.CUSTOM_NAME);
         if (name != null && name.getString().equals("Skeleton Bow")) {
