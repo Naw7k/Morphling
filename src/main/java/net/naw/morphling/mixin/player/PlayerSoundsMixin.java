dc02955deed8de9a05531e6da12c7326539d9860
Naw7k <n_m_k0@hotmail.com>
2026-05-24 02:32:35 +0300

    Update PlayerSoundsMixin.java

--- src/main/java/net/naw/morphling/mixin/player/PlayerSoundsMixin.java
+++ src/main/java/net/naw/morphling/mixin/player/PlayerSoundsMixin.java
@@ -19,17 +19,13 @@ public abstract class PlayerSoundsMixin {
 
     @Inject(method = "getHurtSound", at = @At("HEAD"), cancellable = true)
     private void morphling$overrideHurtSound(DamageSource source, CallbackInfoReturnable<SoundEvent> cir) {
-
-
         LivingEntity self = (LivingEntity)(Object)this;
         if (!(self instanceof Player)) return;
         if (Minecraft.getInstance().player == null) return;
         if (!self.getUUID().equals(Minecraft.getInstance().player.getUUID())) return;
         if (!MorphState.isMorphed()) return;
-
         Entity morphEntity = MorphState.getCachedEntity();
         if (!(morphEntity instanceof LivingEntity livingMorph)) return;
-
         SoundEvent morphHurt = ((LivingEntityHurtSoundAccessor) livingMorph).morphling$getHurtSound(source);
         if (morphHurt != null) cir.setReturnValue(morphHurt);
     }
@@ -41,10 +37,8 @@ public abstract class PlayerSoundsMixin {
         if (Minecraft.getInstance().player == null) return;
         if (!self.getUUID().equals(Minecraft.getInstance().player.getUUID())) return;
         if (!MorphState.isMorphed()) return;
-
         Entity morphEntity = MorphState.getCachedEntity();
         if (!(morphEntity instanceof LivingEntity livingMorph)) return;
-
         SoundEvent morphDeath = ((LivingEntityDeathSoundAccessor) livingMorph).morphling$getDeathSound();
         if (morphDeath != null) cir.setReturnValue(morphDeath);
     }
