feb0bb1d7c628d07c4d338ca59c4926223e56b0a
Naw7k <n_m_k0@hotmail.com>
2026-06-13 15:27:46 +0300

    Add files via upload

---
+++ src/main/java/net/naw/morphling/mixin/mob_ai/ChickenFearFoxMorphMixin.java
@@ -0,0 +1,33 @@
+package net.naw.morphling.mixin.mob_ai;
+
+import net.minecraft.client.Minecraft;
+import net.minecraft.world.entity.ai.goal.AvoidEntityGoal;
+import net.minecraft.world.entity.animal.chicken.Chicken;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.entity.EntityType;
+import net.naw.morphling.client.abilities.FoxAbility;
+import net.naw.morphling.client.core.MorphState;
+import net.naw.morphling.mixin.accessors.MobGoalSelectorAccessor;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(Chicken.class)
+public abstract class ChickenFearFoxMorphMixin {
+
+    @Inject(method = "registerGoals", at = @At("TAIL"))
+    private void morphling$addFearFoxGoal(CallbackInfo ci) {
+        Chicken chicken = (Chicken)(Object) this;
+        ((MobGoalSelectorAccessor) chicken).morphling$getGoalSelector().addGoal(2, new AvoidEntityGoal<>(
+                chicken,
+                Player.class,
+
+                (_) -> MorphState.getCurrentMorph() == EntityType.FOX
+                        && !FoxAbility.isCrouching()
+                        && !(Minecraft.getInstance().player != null && Minecraft.getInstance().player.isCrouching()),
+                8.0F, 1.4, 1.6,
+                (_) -> true
+        ));
+    }
+}
